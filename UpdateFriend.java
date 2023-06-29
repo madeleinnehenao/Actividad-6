2aa4db7047233cda79cbc5011577b021593b0124
madeleinnehenao <124703743+madeleinnehenao@users.noreply.github.com>
2023-06-28 22:57:43 -0500

    Add files via upload

---
+++ UpdateFriend.java
@@ -0,0 +1,113 @@
+
+package archivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.NumberFormatException;
+
+public class UpdateFriend {
+
+    public static void main(String data[]){
+        try{
+            String newName = data[0];
+ 
+            long newNumber = Long.parseLong(data[1]);
+ 
+            String nameNumberString;
+            String name;
+            long number;
+            int index;
+
+            File file = new File("friendsContact.txt");
+ 
+            if (!file.exists()) {
+ 
+                file.createNewFile();
+            }
+            RandomAccessFile raf= new RandomAccessFile(file, "rw");
+            boolean found = false;
+
+            while (raf.getFilePointer() < raf.length()) {
+ 
+                nameNumberString = raf.readLine();
+
+                String[] lineSplit
+                    = nameNumberString.split("!");
+ 
+                name = lineSplit[0];
+                number = Long.parseLong(lineSplit[1]);
+ 
+                if (name == newName
+                    || number == newNumber) {
+                    found = true;
+                    break;
+                }
+            }
+ 
+            if (found == true) {
+
+                File tmpFile = new File("temp.txt");
+ 
+                RandomAccessFile tmpraf
+                    = new RandomAccessFile(tmpFile, "rw");
+ 
+                raf.seek(0);
+ 
+                while (raf.getFilePointer()
+                       < raf.length()) {
+ 
+                    nameNumberString = raf.readLine();
+ 
+                    index = nameNumberString.indexOf('!');
+                    name = nameNumberString.substring(
+                        0, index);
+ 
+                    if (name.equals(newName)) {
+                        nameNumberString
+                            = name + "!"
+                              + String.valueOf(newNumber);
+                    }
+ 
+                    tmpraf.writeBytes(nameNumberString);
+ 
+                    tmpraf.writeBytes(
+                        System.lineSeparator());
+                }
+ 
+                raf.seek(0);
+                tmpraf.seek(0);
+ 
+                while (tmpraf.getFilePointer()
+                       < tmpraf.length()) {
+                    raf.writeBytes(tmpraf.readLine());
+                    raf.writeBytes(System.lineSeparator());
+                }
+ 
+                raf.setLength(tmpraf.length());
+ 
+                tmpraf.close();
+                raf.close();
+ 
+                tmpFile.delete();
+ 
+                System.out.println(" Friend updated. ");
+            }
+            else {
+                raf.close();
+                System.out.println(" Input name"
+                                   + " does not exists. ");
+            }
+        }
+        catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+ 
+        catch (NumberFormatException nef) {
+            System.out.println(nef);
+        }
+        catch (ArrayIndexOutOfBoundsException aioobe) {
+            System.out.println("No se proporcionaron suficientes argumentos.");
+        }
+    }
+}
